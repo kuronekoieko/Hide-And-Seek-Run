c0dc933f2ea342443454f2db3f952973bb5a79cb
家子智成 <>
2021-06-13 11:55:05 +0900

    移動処理(仮)

--- Assets/0_MyAssets/Scripts/Game/PlayerController.cs
+++ Assets/0_MyAssets/Scripts/Game/PlayerController.cs
@@ -2,17 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState
+{
+    Stop,
+    Running,
+    Crouching,
+}
+
+
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Rigidbody rb;
+    PlayerState playerState;
+    float runningSpeed = 5f;
+
     void Start()
     {
-
+        playerState = PlayerState.Stop;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        switch (playerState)
+        {
+            case PlayerState.Stop:
+                if (Input.GetMouseButtonDown(0))
+                {
+                    playerState = PlayerState.Running;
+                }
+                break;
+            case PlayerState.Running:
+                if (Input.GetMouseButtonDown(0))
+                {
+                    playerState = PlayerState.Crouching;
+                }
+                break;
+            case PlayerState.Crouching:
+                if (Input.GetMouseButtonUp(0))
+                {
+                    playerState = PlayerState.Running;
+                }
+                break;
+            default:
+                break;
+        }
+    }
 
+    private void FixedUpdate()
+    {
+        switch (playerState)
+        {
+            case PlayerState.Stop:
+                rb.velocity = Vector3.zero;
+                break;
+            case PlayerState.Running:
+                rb.velocity = Vector3.right * runningSpeed;
+                break;
+            case PlayerState.Crouching:
+                rb.velocity = Vector3.zero;
+                break;
+            default:
+                break;
+        }
     }
 }
